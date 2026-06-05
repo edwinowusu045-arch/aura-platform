866ef11a54904a51e0b158c3a6a683a747338dad
edwinowusu045-arch <edwinowusu045@gmail.com>
2026-06-05 15:52:57 +0000

    Use CommonJS Next.js config and remove outdated ESM config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,9 +1,6 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
+const path = require('path');
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -25,4 +22,4 @@ const nextConfig = {
   }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
